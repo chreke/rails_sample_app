0476486ce48d998606709b3505ef7b00b2d03b3f
JohnyTex <ekeroth.christoffer@gmail.com>
2012-02-26 14:23:03 +0100

    Update tests for static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,29 +5,36 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have a 'Sample App' header" do
-      visit '/static_pages/home'
+      visit home_path
       page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial")
+      visit home_path
+      page.should have_selector('title', :text => "Ruby on Rails Sample App")
     end
 
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
   end
 
   describe "About page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Us')
     end
   end
 
+  describe "Contact page" do
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
+    end
+  end
+  
 end
