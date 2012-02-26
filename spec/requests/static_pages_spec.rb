24c2f3d049341abca19d9650fab27c4f37a9d78a
JohnyTex <ekeroth.christoffer@gmail.com>
2012-02-26 15:26:58 +0100

    Make tests less verbose

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,40 +1,33 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  subject { page }
 
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have a 'Sample App' header" do
-      visit home_path
-      page.should have_selector('h1', :text => 'Sample App')
-    end
-
-    it "should have the right title" do
-      visit home_path
-      page.should have_selector('title', :text => "Ruby on Rails Sample App")
-    end
+    
+    it { should have_selector('h1', text: 'Sample App') }
+    it { should have_selector('title', :text => "Ruby on Rails Sample App") }
 
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit help_path
-      page.should have_content('Help')
-    end
+    before { visit help_path }
+
+    it { should have_content('Help') }
   end
 
   describe "About page" do
-    it "should have the content 'About Us'" do
-      visit about_path
-      page.should have_content('About Us')
-    end
+    before { visit about_path }
+
+    it { should have_content('About Us') }
   end
 
   describe "Contact page" do
-    it "should have the h1 'Contact'" do
-      visit contact_path
-      page.should have_selector('h1', text: 'Contact')
-    end
+    before { visit contact_path }
+
+    it { should have_selector('h1', text: 'Contact') }
   end
   
 end
