2fcfecbd0abe5f3c885c6dcf1da5af2d461b5aff
JohnyTex <ekeroth.christoffer@gmail.com>
2012-02-26 14:27:54 +0100

    Remove default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ SampleApp::Application.routes.draw do
   match '/about',    to: 'static_pages#about'
   match '/contact',  to: 'static_pages#contact'
   match '/help',     to: 'static_pages#help'
-  match '/home',     to: 'static_pages#home'
+
+  root to: 'static_pages#home'
 end
